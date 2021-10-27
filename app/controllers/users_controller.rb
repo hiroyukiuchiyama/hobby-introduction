464ea13a6cbbc7501ee699d73aa50580f9660347
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-10-27 04:51:14 +0000

    hobbyintroduction controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+    before_action :ensure_correct_user, only: [:edit,:update,:destroy]
+    
+    def show
+        @user = User.find(params[:id])
+        @hobbys = @user.hobbys
+        @hobby = Hobby.new
+    end
+    
+    def index
+        @user = User.all
+        @hobby = Hobby.new
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def updae
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+         redirect_to user_path(@user), notice: "You have updated user successfully."
+        else
+         render 'edit'
+        end
+    end
+    
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :introduction, :profil_image)
+    end
+    
+    def ensure_correct_user
+        @user = User.find(params[:id])
+        unless @user == current_user
+            redirect_to user _path(current_user)
+        end
+    end
+    
+        
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name,length: { in: 2..20 }
+  validates :name, uniqueness: true
+  validates :introduction, length: { maximum: 150 }
+  validates :name, presence: true
+  validates :name,length: { in: 2..20 }
+  validates :name, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  attachment :profile_image
+  
+  has_many :hobby
+  
+  
 end
