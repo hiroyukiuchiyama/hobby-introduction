3808fbefd212cdcca9c5fb5a0084fa85757c1648
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-11-15 14:15:07 +0000

    画像投稿機能追加1

--- app/controllers/hobby_image_controller.rb
+++ app/controllers/hobby_image_controller.rb
@@ -1,7 +1,29 @@
 class HobbyImageController < ApplicationController
   def new
+     @hobby_image = HobbyIntroduction.new
   end
 
   def create
+    @hobby_image = HobbyImage.new(post_image_params)
+    @hobby_image.user_id = current_user.id
+    @hobby_image.save
+    redirect_to hobby_image_path
+  end
+  
+  def index
+    @hobby_image = HobbyImage.all
+  end
+
+  def show
+    @hobby_image = HobbyImage.find(params[:id])
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def hooby_image_params
+    params.require(:hobby_image).permit(:image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users,only: [:index,:show,:edit,:update]
   resources :hobbys
+   resources :hobby_image, only: [:new, :create, :index, :show, :destroy]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
